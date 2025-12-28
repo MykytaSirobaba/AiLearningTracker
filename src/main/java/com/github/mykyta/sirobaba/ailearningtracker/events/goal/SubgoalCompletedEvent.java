28cda132d3b92057f8c06800ef8839a82f8cf50e
mykytasirobaba <mykyta.sirobaba@gmail.com>
2025-12-28 15:18:26 +0200

    doc: Add Java documentation to SubgoalCompletedEvent

--- src/main/java/com/github/mykyta/sirobaba/ailearningtracker/events/goal/SubgoalCompletedEvent.java
+++ src/main/java/com/github/mykyta/sirobaba/ailearningtracker/events/goal/SubgoalCompletedEvent.java
@@ -1,6 +1,17 @@
 package com.github.mykyta.sirobaba.ailearningtracker.events.goal;
 
+
 /**
+ * Event triggered when a subgoal is completed.
+ * <p>
+ * Contains the ID of the parent goal to which the completed subgoal belongs.
+ * This event can be used to perform actions such as updating the parent goal's
+ * completion status or notifying other parts of the system.
+ * </p>
+ *
+ * @param parentGoalId the ID of the parent goal associated with the completed subgoal
+ *
+ * <p>
  * Created by Mykyta Sirobaba on 28.10.2025.
  * email mykyta.sirobaba@gmail.com
  */
